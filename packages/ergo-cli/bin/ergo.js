d62368ea86d7c92b8145cf3022906327fe6e19e9
Jerome Simeon <jeromesimeon@me.com>
2018-04-24 13:08:02 -0400

    (feature) Separate compiler from CLI
    Signed-off-by: Jerome Simeon <jeromesimeon@me.com>

---
+++ packages/ergo-cli/bin/ergo.js
@@ -0,0 +1,97 @@
+#!/usr/bin/env node
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+'use strict';
+
+const Commands = require('../lib/commands');
+const Logger = require('@accordproject/ergo-compiler/lib/logger');
+
+require('yargs')
+    .command('compile', 'compile Ergo to JavaScript', (yargs) => {
+        yargs.option('ergo', {
+            describe: 'path to the Ergo source'
+        });
+        yargs.option('cto', {
+            describe: 'path to CTO models'
+        }).array('cto');
+        yargs.option('contractname', {
+            describe: 'contract name'
+        });
+        yargs.option('clausename', {
+            describe: 'clause name'
+        });
+        yargs.option('dispatch', {
+            describe: 'generate dispatch function',
+            type: 'boolean',
+            default: false
+        });
+    }, (argv) => {
+        if (argv.verbose) {
+            Logger.info(`compile Ergo file ${argv.ergo} with contract ${argv.contractname} and clause with contract ${argv.clausename} and CTOs ${argv.cto}`);
+        }
+        return Commands.compile(argv.ergo,argv.cto,argv.contractname,argv.clausename, argv.dispatch)
+            .then((result) => {
+                Logger.info(result);
+            })
+            .catch((err) => {
+                Logger.error(err.message + ' ' + JSON.stringify(err));
+            });
+    })
+    .command('execute', 'execute a contract clause', (yargs) => {
+        yargs.option('contract', {
+            describe: 'path to the contract data'
+        });
+        yargs.option('request', {
+            describe: 'path to the request data'
+        });
+        yargs.option('state', {
+            describe: 'path to the state data'
+        });
+        yargs.option('ergo', {
+            describe: 'path to the Ergo file'
+        });
+        yargs.option('cto', {
+            describe: 'path to CTO models'
+        }).array('cto');
+        yargs.option('contractname', {
+            describe: 'contract name'
+        });
+        yargs.option('clausename', {
+            describe: 'clause name'
+        });
+        yargs.option('dispatch', {
+            describe: 'generate dispatch function',
+            type: 'boolean',
+            default: false
+        });
+    }, (argv) => {
+        if (argv.verbose) {
+            Logger.info(`execute Ergo file ${argv.ergo} on contract data ${argv.contract} with request data ${argv.request} and CTOs ${argv.cto}`);
+        }
+
+        return Commands.execute(argv.ergo, argv.cto, argv.contract, argv.request, argv.state, argv.contractname, argv.clausename, argv.dispatch)
+            .then((result) => {
+                Logger.info(JSON.stringify(result));
+            })
+            .catch((err) => {
+                Logger.error(err.message + ' ' + JSON.stringify(err));
+            });
+    })
+    .demandCommand()
+    .option('verbose', {
+        alias: 'v',
+        default: false
+    })
+    .argv;
