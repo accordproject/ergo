1ad8a700b56660c3c28188f077c1a8dc0375fcd0
Jerome Simeon <jeromesimeon@me.com>
2018-04-26 13:49:44 -0400

    (chore) Up 0.0.41
    Signed-off-by: Jerome Simeon <jeromesimeon@me.com>

--- mechanization/Version.v
+++ mechanization/Version.v
@@ -17,8 +17,7 @@
 Require Import String.
 
 Section Version.
-  (** Variables are defined as strings *)
-  Definition ergo_version := "0.0.40"%string.
+  Definition ergo_version := "0.0.41"%string.
 
 End Version.
 
