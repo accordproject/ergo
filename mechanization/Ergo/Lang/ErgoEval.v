b296bee2679a7d0214b49613c4f1d6843a1da2b2
kach <hardmath123@gmail.com>
2018-06-25 16:19:52 -0400

    (feat) multiple exprs in module; globals; new kwd
    Signed-off-by: kach <hardmath123@gmail.com>

--- mechanization/Ergo/Lang/ErgoEval.v
+++ mechanization/Ergo/Lang/ErgoEval.v
@@ -32,44 +32,57 @@ Section ErgoEval.
 
   Record ergo_context :=
     mkContext
-      { function_list : list (string * ergo_function);
-        clause_list : list ergo_clause;
-        contract_list : list ergo_contract;
-        environment_list : list (string * ergo_data);
-        this_clause : ergo_data;
-        this_contract : ergo_data;
-        this_state : ergo_data;
-        this_emit : ergo_data;
+      { ctx_function_list : list (string * ergo_function);
+        ctx_clause_list : list ergo_clause;
+        ctx_contract_list : list ergo_contract;
+        ctx_global_env : list (string * ergo_data);
+        ctx_local_env : list (string * ergo_data);
+        ctx_this_clause : ergo_data;
+        ctx_this_contract : ergo_data;
+        ctx_this_state : ergo_data;
+        ctx_this_emit : ergo_data;
         }.
 
   Definition ergo_empty_context :=
-    mkContext nil nil nil nil dunit dunit dunit dunit.
+    mkContext nil nil nil nil nil dunit dunit dunit dunit.
 
   Definition TODO {A : Set} : eresult A := efailure (SystemError "Feature not implemented.").
 
   Definition ergo_unary_eval := ErgoOps.Unary.eval.
   Definition ergo_binary_eval := ErgoOps.Binary.eval.
 
-  Definition ergo_context_update_env (ctx : ergo_context) (name : string) (value : ergo_data) : ergo_context :=
-    mkContext ctx.(function_list)
-              ctx.(clause_list)
-              ctx.(contract_list)
-              ((name, value)::ctx.(environment_list))
-              ctx.(this_clause)
-              ctx.(this_contract)
-              ctx.(this_state)
-              ctx.(this_emit).
+  Definition ergo_context_update_global_env (ctx : ergo_context) (name : string) (value : ergo_data) : ergo_context :=
+    mkContext ctx.(ctx_function_list)
+              ctx.(ctx_clause_list)
+              ctx.(ctx_contract_list)
+              ((name, value)::ctx.(ctx_global_env))
+              (ctx.(ctx_local_env))
+              ctx.(ctx_this_clause)
+              ctx.(ctx_this_contract)
+              ctx.(ctx_this_state)
+              ctx.(ctx_this_emit).
 
-  Definition ergo_context_update_env' (ctx : ergo_context) (items : list (string * ergo_data)) : ergo_context :=
-    fold_left (fun ctx' item => ergo_context_update_env ctx' (fst item) (snd item)) items ctx.
+  Definition ergo_context_update_local_env (ctx : ergo_context) (name : string) (value : ergo_data) : ergo_context :=
+    mkContext ctx.(ctx_function_list)
+              ctx.(ctx_clause_list)
+              ctx.(ctx_contract_list)
+              (ctx.(ctx_global_env))
+              ((name, value)::ctx.(ctx_local_env))
+              ctx.(ctx_this_clause)
+              ctx.(ctx_this_contract)
+              ctx.(ctx_this_state)
+              ctx.(ctx_this_emit).
+
+  Definition ergo_context_update_local_env' (ctx : ergo_context) (items : list (string * ergo_data)) : ergo_context :=
+    fold_left (fun ctx' item => ergo_context_update_local_env ctx' (fst item) (snd item)) items ctx.
 
 Fixpoint ergo_eval_expr (ctx : ergo_context) (expr : ergo_expr) : eresult ergo_data :=
   match expr with
-  | EThisContract => esuccess ctx.(this_contract)
-  | EThisClause => esuccess ctx.(this_clause)
-  | EThisState => esuccess ctx.(this_state)
+  | EThisContract => esuccess ctx.(ctx_this_contract)
+  | EThisClause => esuccess ctx.(ctx_this_clause)
+  | EThisState => esuccess ctx.(ctx_this_state)
   | EVar name =>
-    let opt := lookup String.string_dec ctx.(environment_list) name in
+    let opt := lookup String.string_dec (ctx.(ctx_local_env)++ctx.(ctx_global_env)) name in
     eresult_of_option opt (RuntimeError ("Variable not found: " ++ name))
   | EConst d => esuccess d
 
@@ -124,7 +137,7 @@ Fixpoint ergo_eval_expr (ctx : ergo_context) (expr : ergo_expr) : eresult ergo_d
   | ELet n t v e =>
     match ergo_eval_expr ctx v with
     | Success _ _ v' =>
-      let ctx' := ergo_context_update_env ctx n v' in
+      let ctx' := ergo_context_update_local_env ctx n v' in
       ergo_eval_expr ctx' e
     | Failure _ _ f => efailure f
     end
@@ -146,9 +159,36 @@ Fixpoint ergo_eval_expr (ctx : ergo_context) (expr : ergo_expr) : eresult ergo_d
          end)
       rs (esuccess (drec nil))
 
-  | ENew nr vs => esuccess dunit
+      (* RIP modularity *)
+  | ENew nr rs =>
+    match nr with
+      (* TODO this is wrong, they should be flipped... >sigh< *)
+    | ENames.AbsoluteRef n => efailure (SystemError "Name resolution did not resolve name.")
+    | ENames.RelativeRef _ n =>
+      match
+        fold_left
+          (fun ls nv =>
+             let name := fst nv in
+             let value := snd nv in
+             match ls with
+             | Success _ _ (drec ls') =>
+               match ergo_eval_expr ctx value with
+               (* TODO OpRecConcat to normalize shadowing properly *)
+               | Success _ _ value' => esuccess (drec (ls' ++ ((name, value')::nil)))
+               | Failure _ _ f => efailure f
+               end
+             | Success _ _ _ => efailure (RuntimeError "This should never happen.")
+             | Failure _ _ f => efailure f
+             end)
+          rs (esuccess (drec nil))
+      with
+      | Failure _ _ f => efailure f
+      | Success _ _ r => esuccess (dbrand (n::nil) r)
+      end
+    end
+
   | ECallFun fn args =>
-    match lookup String.string_dec ctx.(function_list) fn with
+    match lookup String.string_dec ctx.(ctx_function_list) fn with
     | Some fn' =>
       let lam := fn'.(function_lambda) in
       let nms := lam.(lambda_params) in
@@ -183,16 +223,28 @@ Fixpoint ergo_eval_decl (ctx : ergo_context) (decl : ergo_declaration) : eresult
     | Success _ _ r => esuccess (ctx, Some r)
     | Failure _ _ f => efailure f
     end
-  | EGlobal n e => TODO
+  | EGlobal n e =>
+    match ergo_eval_expr ctx e with
+    | Success _ _ r =>
+      esuccess (ergo_context_update_global_env ctx n r, Some r)
+    | Failure _ _ f => efailure f
+    end
   | EFunc f => TODO
   | EContract c => TODO
   end.
 
 Definition ergo_eval_module (ctx : ergo_context) (module : ergo_module) : eresult (ergo_context * option ergo_data) :=
-  match (module.(module_declarations)) with
-  | nil => TODO
-  | d::ds => ergo_eval_decl ctx d
-  end.
+  fold_left
+    (fun prev_ctx d =>
+       match prev_ctx with
+       | Failure _ _ f => efailure f
+       | Success _ _ (ctx', _) =>
+         match ergo_eval_decl ctx' d with
+         | Failure _ _ f => efailure f
+         | s => s
+         end
+       end)
+  module.(module_declarations) (esuccess (ctx, None)).
 
 Definition ergo_string_of_error (err : eerror) : string :=
   match err with
@@ -205,7 +257,7 @@ Definition ergo_string_of_error (err : eerror) : string :=
 
 Definition ergo_string_of_result (result : eresult (ergo_context * option ergo_data)) : string :=
   match result with
-  | Success _ _ (ctx, None) => "oops"
+  | Success _ _ (ctx, None) => "[ok]"
   | Success _ _ (ctx, Some d) => dataToString d
   | Failure _ _ f => ergo_string_of_error f
   end.
@@ -219,6 +271,7 @@ Definition ergo_maybe_update_context (ctx : ergo_context) (result : eresult (erg
 
 End ErgoEval.
 
+(*
 Section Tests.
 
   Definition cow : string := "disco".
@@ -235,3 +288,5 @@ Section Tests.
   Compute (ergo_eval_expr ergo_empty_context records).
 
 End Tests.
+
+*)
