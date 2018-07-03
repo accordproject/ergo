5466fe0101e1ec84e59ce4935b065a650004f792
kach <hardmath123@gmail.com>
2018-07-03 16:51:10 -0400

    (cleanup) inlining is its own module now yay
    Signed-off-by: kach <hardmath123@gmail.com>

--- mechanization/Ergo/Lang/Ergo.v
+++ mechanization/Ergo/Lang/Ergo.v
@@ -232,5 +232,68 @@ Section Ergo.
              (lookup_single_contract_in_declarations p.(module_annot) p.(module_declarations)).
   End Lookup.
 
+
+Section ErgoContext.
+
+Definition ergo_data := ErgoData.data.
+  Record ergo_context :=
+    mkContext
+      { ctx_function_env : list (string * laergo_function);
+        ctx_clause_env : list (string * laergo_clause);
+        ctx_contract_env : list (string * laergo_contract);
+        ctx_global_env : list (string * ergo_data);
+        ctx_local_env : list (string * ergo_data);
+        ctx_this_clause : ergo_data;
+        ctx_this_contract : ergo_data;
+        ctx_this_state : ergo_data;
+        ctx_this_emit : ergo_data;
+        }.
+
+  Definition ergo_ctx_update_function_env (ctx : ergo_context) (name : string) (value : ergo_function) : ergo_context :=
+    mkContext ((name, value)::ctx.(ctx_function_env))
+              ctx.(ctx_clause_env)
+              ctx.(ctx_contract_env)
+              (ctx.(ctx_global_env))
+              ctx.(ctx_local_env)
+              ctx.(ctx_this_clause)
+              ctx.(ctx_this_contract)
+              ctx.(ctx_this_state)
+              ctx.(ctx_this_emit).
+
+  Definition ergo_ctx_update_global_env (ctx : ergo_context) (name : string) (value : ergo_data) : ergo_context :=
+    mkContext ctx.(ctx_function_env)
+              ctx.(ctx_clause_env)
+              ctx.(ctx_contract_env)
+              ((name, value)::ctx.(ctx_global_env))
+              (ctx.(ctx_local_env))
+              ctx.(ctx_this_clause)
+              ctx.(ctx_this_contract)
+              ctx.(ctx_this_state)
+              ctx.(ctx_this_emit).
+
+  Definition ergo_ctx_update_local_env (ctx : ergo_context) (name : string) (value : ergo_data) : ergo_context :=
+    mkContext ctx.(ctx_function_env)
+              ctx.(ctx_clause_env)
+              ctx.(ctx_contract_env)
+              (ctx.(ctx_global_env))
+              ((name, value)::ctx.(ctx_local_env))
+              ctx.(ctx_this_clause)
+              ctx.(ctx_this_contract)
+              ctx.(ctx_this_state)
+              ctx.(ctx_this_emit).
+
+  Definition ergo_ctx_set_local_env (ctx : ergo_context) (new_local_env : list (string * ergo_data)) : ergo_context :=
+    mkContext ctx.(ctx_function_env)
+              ctx.(ctx_clause_env)
+              ctx.(ctx_contract_env)
+              (ctx.(ctx_global_env))
+              new_local_env
+              ctx.(ctx_this_clause)
+              ctx.(ctx_this_contract)
+              ctx.(ctx_this_state)
+              ctx.(ctx_this_emit).
+
+End ErgoContext.
+
 End Ergo.
 
