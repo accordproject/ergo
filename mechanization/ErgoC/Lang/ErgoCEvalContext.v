3139ad4f06649b93d5859b732d30479c43925ec7
Jerome Simeon <jeromesimeon@me.com>
2018-07-12 18:57:23 -0400

    (fix) Remove unused part of eval context; Set initial contract/state/now to nil
    Signed-off-by: Jerome Simeon <jeromesimeon@me.com>

--- mechanization/ErgoC/Lang/ErgoCEvalContext.v
+++ mechanization/ErgoC/Lang/ErgoCEvalContext.v
@@ -26,10 +26,6 @@ Section ErgocEvalContext.
       { ctx_function_env : list (string * ergoc_function);
         ctx_global_env : list (string * ergo_data);
         ctx_local_env : list (string * ergo_data);
-        ctx_this_clause : ergo_data;
-        ctx_this_contract : ergo_data;
-        ctx_this_state : ergo_data;
-        ctx_this_emit : ergo_data;
       }.
 
   Definition ergo_ctx_update_function_env
@@ -38,11 +34,7 @@ Section ErgocEvalContext.
              (value : ergoc_function) : eval_context :=
     mkEvalContext ((name, value)::ctx.(ctx_function_env))
                   ctx.(ctx_global_env)
-                  ctx.(ctx_local_env)
-                  ctx.(ctx_this_clause)
-                  ctx.(ctx_this_contract)
-                  ctx.(ctx_this_state)
-                  ctx.(ctx_this_emit).
+                  ctx.(ctx_local_env).
 
   Definition ergo_ctx_update_global_env
              (ctx : eval_context)
@@ -50,11 +42,7 @@ Section ErgocEvalContext.
              (value : ergo_data) : eval_context :=
     mkEvalContext ctx.(ctx_function_env)
                   ((name, value)::ctx.(ctx_global_env))
-                  ctx.(ctx_local_env)
-                  ctx.(ctx_this_clause)
-                  ctx.(ctx_this_contract)
-                  ctx.(ctx_this_state)
-                  ctx.(ctx_this_emit).
+                  ctx.(ctx_local_env).
 
   Definition ergo_ctx_update_local_env
              (ctx : eval_context)
@@ -62,34 +50,22 @@ Section ErgocEvalContext.
              (value : ergo_data) : eval_context :=
     mkEvalContext ctx.(ctx_function_env)
                   ctx.(ctx_global_env)
-                  ((name, value)::ctx.(ctx_local_env))
-                  ctx.(ctx_this_clause)
-                  ctx.(ctx_this_contract)
-                  ctx.(ctx_this_state)
-                  ctx.(ctx_this_emit).
+                  ((name, value)::ctx.(ctx_local_env)).
 
   Definition ergo_ctx_set_local_env
              (ctx : eval_context)
              (new_local_env : list (string * ergo_data)) : eval_context :=
     mkEvalContext ctx.(ctx_function_env)
                   ctx.(ctx_global_env)
-                  new_local_env
-                  ctx.(ctx_this_clause)
-                  ctx.(ctx_this_contract)
-                  ctx.(ctx_this_state)
-                  ctx.(ctx_this_emit).
+                  new_local_env.
 
   Definition ergo_empty_context :=
     mkEvalContext nil nil
-                  (("contract"%string, dcoll nil)
-                     ::("state"%string, dcoll nil)
+                  (("contract"%string, dunit)
+                     ::("state"%string, dunit)
                      ::("emit"%string, dcoll nil)
-                     ::("response"%string, dcoll nil)
-                     ::("lstate"%string, dcoll nil)
-                     ::("lemit"%string, dcoll nil)
-                     ::("now"%string, dcoll nil)
-                     ::nil)
-                  dunit dunit dunit dunit.
+                     ::("now"%string, dunit)
+                     ::nil).
   
 End ErgocEvalContext.
 
