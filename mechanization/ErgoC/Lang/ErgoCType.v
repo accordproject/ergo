1011e8f695e7f5cfbf041a5650741265c9e426bf
kach <hardmath123@gmail.com>
2018-07-26 18:29:51 -0400

    (fix) seems like the answer here is "any" rather than "nothing", because "any" works and "nothing" does not
    Signed-off-by: kach <hardmath123@gmail.com>

--- mechanization/ErgoC/Lang/ErgoCType.v
+++ mechanization/ErgoC/Lang/ErgoCType.v
@@ -64,7 +64,7 @@ Section ErgoCType.
                      (fun new' => ergoc_type_meet T' new')
                      (ergo_type_expr ctxt new))
                 T)
-           es (esuccess (tcoll tbottom)))
+           es (esuccess ttop))
     | EUnaryOp prov op e =>
       match ergo_type_expr ctxt e with
       | Success _ _ e' =>
