15ddc8968b2fbda7ccca2f80e0cdd0490118b3c9
kach <hardmath123@gmail.com>
2018-08-06 11:39:18 -0400

    (fix) colors
    Signed-off-by: kach <hardmath123@gmail.com>

--- mechanization/Common/Utils/EData.v
+++ mechanization/Common/Utils/EData.v
@@ -12,6 +12,7 @@
  * limitations under the License.
  *)
 
+Require Import Ascii.
 Require Import String.
 Require Import List.
 Require Import ErgoSpec.Backend.ErgoBackend.
@@ -32,6 +33,9 @@ Section EData.
       Some (response, emits, state)
     | _ => None
     end.
+
+  Definition fmt_nl := String.String (ascii_of_N 10) EmptyString.
+  Definition fmt_dq := """"%string.
 
   Fixpoint string_of_data (d : ergo_data) : string :=
     let jsonify := ErgoData.data_to_json_string fmt_dq in
@@ -67,45 +71,39 @@ Section EData.
     end.
 
   Definition string_of_response (response : ergo_data) : string :=
-    (fmt_grn "Response. ") ++ (string_of_data response) ++ fmt_nl.
+    "Response. " ++ (string_of_data response) ++ fmt_nl.
 
   Definition string_of_emits (emits : list ergo_data) : string :=
     (fold_left
-       (fun old new => ((fmt_mag "Emit. ") ++ new ++ fmt_nl ++ old)%string)
+       (fun old new => ("Emit. " ++ new ++ fmt_nl ++ old)%string)
        (map (string_of_data) emits) ""%string).
 
   Definition string_of_state (old_state : option ergo_data) (new_state : ergo_data)
     : string :=
     let jsonify := string_of_data in
     match old_state with
-    | None => (fmt_blu "State. ") ++ (jsonify new_state)
+    | None => "State. " ++ (jsonify new_state)
     | Some actual_old_state =>
       if Data.data_eq_dec new_state actual_old_state then
         ""%string
       else
-        (fmt_blu "State. ") ++ (jsonify new_state) ++ fmt_nl
+        "State. " ++ (jsonify new_state) ++ fmt_nl
     end.
 
-    (* XXX May be nice to replace by a format that aligns with Ergo notations instead of JSON and move to an earlier module e.g., Common/Utils/EData *)
     Definition string_of_result_data (old_state : option ergo_data) (result : option ergo_data)
       : string :=
       match result with
       | None => ""
       | Some (dright msg) =>
-        fmt_red ("Error. "%string) ++ (string_of_data msg) ++ fmt_nl
+        "Error. "%string ++ (string_of_data msg) ++ fmt_nl
       | Some out =>
         match unpack_output out with
         | Some (response, emits, state) =>
           (string_of_emits emits) ++ (string_of_response response) ++ (string_of_state old_state state)
         | None => (string_of_data out) ++ fmt_nl
         end
-(* Note: this was previously powered by QCert's dataToString d, and I kind of
-   liked that better anyway, so we might transition back at some point. The
-   problem was that QCert treated arrays as bags and sorted them before
-   printing (!!!). *)
       end.
 
-    (* XXX May be nice to replace by a format that aligns with Ergo notations instead of JSON and move to an earlier module e.g., Common/Utils/EData *)
     Definition string_of_result_type (result : option ergoc_type)
       : string :=
       match result with
