3e793bbc5d83b7fd6f51c165173d11266777f669
kach <hardmath123@gmail.com>
2018-08-13 16:01:34 -0700

    (fix) license
    Signed-off-by: kach <hardmath123@gmail.com>

--- packages/ergo-cli/lib/ergo-mode.js
+++ packages/ergo-cli/lib/ergo-mode.js
@@ -1,3 +1,17 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 CodeMirror.defineSimpleMode("ergo", {
   start: [
     {regex: /"(?:[^\\]|\\.)*?(?:"|$)/, token: "string"},
@@ -16,18 +30,12 @@ CodeMirror.defineSimpleMode("ergo", {
     {regex: /[\}\]\)]/, dedent: true},
     {regex: /\w+/, token: "variable"},
   ],
-  // The multi-line comment state.
   comment: [
     {regex: /.*?\*\//, token: "comment", next: "start"},
     {regex: /.*/, token: "comment"}
   ],
-  // The meta property contains global information about the mode. It
-  // can contain properties like lineComment, which are supported by
-  // all modes, and also directives like dontIndentStates, which are
-  // specific to simple modes.
   meta: {
     dontIndentStates: ["comment"],
     lineComment: "//"
   }
 });
-
