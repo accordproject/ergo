9fe1be805785191046b9253d683c431913535ecb
Jerome Simeon <jeromesimeon@me.com>
2018-11-12 15:34:23 -0500

    (fix) Ergo logger cleanup
    Signed-off-by: Jerome Simeon <jeromesimeon@me.com>

--- packages/ergo-compiler/lib/logger.js
+++ packages/ergo-compiler/lib/logger.js
@@ -15,33 +15,112 @@
 'use strict';
 
 const winston = require('winston');
+const { LEVEL, MESSAGE } = require('triple-beam');
+const jsonStringify = require('fast-safe-stringify');
+const jsome = require('jsome');
 const fs = require('fs');
 const env = process.env.NODE_ENV || 'development';
 const tsFormat = () => (new Date()).toLocaleTimeString();
 
-const logDir = 'log';
-// Create the log directory if it does not exist
-if (!fs.existsSync(logDir)) {
-    fs.mkdirSync(logDir);
+/**
+ * Helper function to test is a string is a stringified version of a JSON object
+ * @param {string} str - the input string to test
+ * @returns {boolean} - true iff the string can be parsed as JSON
+ * @private
+ */
+function isJSON(str) {
+    try {
+        return (JSON.parse(str) && !!str);
+    } catch (e) {
+        return false;
+    }
 }
 
+jsome.params.lintable = true;
+
+const jsonColor =  winston.format(info => {
+    const padding = info.padding && info.padding[info.level] || '';
+
+    if(info[LEVEL] === 'error' && info.stack) {
+        info[MESSAGE] = `${tsFormat()} - ${info.level}:${padding} ${info.message}\n${info.stack}`;
+        return info;
+    }
+
+    if (info[LEVEL] === 'info') {
+        if(typeof info.message === 'object') {
+            info[MESSAGE] = `${tsFormat()} - ${info.level}:${padding}\n${jsome.getColoredString(info.message, null, 2)}`;
+        } else if(isJSON(info.message)) {
+            info[MESSAGE] =`${tsFormat()} - ${info.level}:${padding}\n${jsome.getColoredString(JSON.parse(info.message), null, 2)}`;
+        } else {
+            info[MESSAGE] = `${tsFormat()} - ${info.level}:${padding} ${info.message}`;
+        }
+        return info;
+    }
+
+    const stringifiedRest = jsonStringify(Object.assign({}, info, {
+        level: undefined,
+        message: undefined,
+        splat: undefined
+    }));
+
+    if (stringifiedRest !== '{}') {
+        info[MESSAGE] = `${tsFormat()} - ${info.level}:${padding} ${info.message} ${stringifiedRest}`;
+    } else {
+        info[MESSAGE] = `${tsFormat()} - ${info.level}:${padding} ${info.message}`;
+    }
+    return info;
+
+});
+
+const enumerateErrorFormat = winston.format(info => {
+    if (info.message instanceof Error) {
+        info.message = Object.assign({
+            message: info.message.message,
+            stack: info.message.stack
+        }, info.message);
+    }
+
+    if (info instanceof Error) {
+        return Object.assign({
+            message: info.message,
+            stack: info.stack
+        }, info);
+    }
+
+    return info;
+});
+
 let logger = winston.createLogger({
+    format: winston.format.combine(
+        winston.format.json(),
+        enumerateErrorFormat(),
+        winston.format.colorize(),
+        jsonColor(),
+    ),
     transports: [
         new winston.transports.Console({
-            colorize: true,
-            timestamp: tsFormat,
-            level: 'info'
+            level: 'info',
         }),
-        new winston.transports.File({
-            name: 'logs-file',
-            filename: `${logDir}/trace.log`,
-            level: env === 'development' ? 'debug' : 'info'
-        })
     ]
 });
+
+// Only write log files to disk if we're running in development
+// and not in a browser (webpack or browserify)
+if(env === 'development' && !process.browser){
+    const logDir = 'log';
+    // Create the log directory if it does not exist
+    if (!fs.existsSync(logDir)) {
+        fs.mkdirSync(logDir);
+    }
+
+    logger.add(new winston.transports.File({
+        name: 'logs-file',
+        filename: `${logDir}/trace.log`,
+        level: env === 'development' ? 'debug' : 'info'
+    }));
+}
 
 logger.entry = logger.debug;
 logger.exit = logger.debug;
 
-logger.log('info', 'Logging initialized.', new Date());
 module.exports = logger;
