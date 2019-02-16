ea0b3620c55ae6592d769fc2ba387ab9034051d9
Jerome Simeon <jeromesimeon@me.com>
2019-02-16 17:33:18 -0500

    (fix) Remove unnecessary AccordClauseLogic annotations
    Signed-off-by: Jerome Simeon <jeromesimeon@me.com>

--- packages/ergo-cli/lib/ergotop-lib.js
+++ packages/ergo-cli/lib/ergotop-lib.js
@@ -21716,7 +21716,7 @@ i=et(c[1]);var
 A=r_(function(a){return a},i);hc(hb,ap(aeo,ap(mK,aen)));function
 C(d,c,b){var
 a=aep(c,fK(b));return{"out":a[1].toString(),"ctx":a[2]}}var
-D={"initRCtxt":A,"version":mK.toString(),"buildate":"Feb 15, 2019","runLine":afh(C)};return afg(0).ergotop=D},aeD=function(d,a){var
+D={"initRCtxt":A,"version":mK.toString(),"buildate":"Feb 16, 2019","runLine":afh(C)};return afg(0).ergotop=D},aeD=function(d,a){var
 b=d[3];if(a[1]===jL)return new
 oN(mJ(b,a[2]).toString());function
 c(l){var
