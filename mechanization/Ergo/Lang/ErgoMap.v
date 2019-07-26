4a8f9d44690ca6d33d33157bfc098b692f35dc3c
Jerome Simeon <jeromesimeon@me.com>
2019-07-25 21:19:41 -0400

    fix(inline) proper local scoping for foreach and inlining code cleanup
    Signed-off-by: Jerome Simeon <jeromesimeon@me.com>

--- mechanization/Ergo/Lang/ErgoMap.v
+++ mechanization/Ergo/Lang/ErgoMap.v
@@ -27,12 +27,13 @@ Section ErgoMap.
   Context {N:Set}. (* For names *)
 
   Fixpoint ergo_map_expr {C : Set}
+           (ctxt_new_variable_scope : C -> string -> @ergo_expr A A' N -> C)      (* Apply when scope changes *)
+           (fn : C -> @ergo_expr A A' N -> option (eresult (@ergo_expr A A' N)))  (* The mapped function *)
            (ctx : C)
-           (ctxt_new_variable_scope : C -> string -> @ergo_expr A A' N -> C)
-           (fn : C -> @ergo_expr A A' N -> option (eresult (@ergo_expr A A' N)))
            (expr : @ergo_expr A A' N)
     : eresult (@ergo_expr A A' N) :=
     let maybe_fn := elift_maybe (fn ctx) in
+    let apply_map := ergo_map_expr ctxt_new_variable_scope fn in
     maybe_fn
       match expr with
       | EThisContract _ => esuccess expr nil
@@ -42,77 +43,85 @@ Section ErgoMap.
       | EConst _ _ => esuccess expr nil
       | ENone _ => esuccess expr nil
       | ESome loc e =>
-        elift (ESome loc) (ergo_map_expr ctx ctxt_new_variable_scope fn e)
+        elift (ESome loc) (apply_map ctx e)
       | EArray loc a =>
         elift (EArray loc)
               (fold_left
                  (fun ls na =>
-                    elift2 postpend ls (ergo_map_expr ctx ctxt_new_variable_scope fn na))
+                    elift2 postpend ls (apply_map ctx na))
                  a (esuccess nil nil))
       | EUnaryOperator loc o e =>
         elift (EUnaryOperator loc o)
-              (ergo_map_expr ctx ctxt_new_variable_scope fn e)
+              (apply_map ctx e)
       | EBinaryOperator loc o e1 e2 =>
         elift2 (EBinaryOperator loc o)
-               (ergo_map_expr ctx ctxt_new_variable_scope fn e1)
-               (ergo_map_expr ctx ctxt_new_variable_scope fn e2)
+               (apply_map ctx e1)
+               (apply_map ctx e2)
       | EUnaryBuiltin loc o e =>
-        elift (EUnaryBuiltin loc o) (ergo_map_expr ctx ctxt_new_variable_scope fn e)
+        elift (EUnaryBuiltin loc o) (apply_map ctx e)
       | EBinaryBuiltin loc o e1 e2 =>
         elift2 (EBinaryBuiltin loc o)
-               (ergo_map_expr ctx ctxt_new_variable_scope fn e1)
-               (ergo_map_expr ctx ctxt_new_variable_scope fn e2)
+               (apply_map ctx e1)
+               (apply_map ctx e2)
       | EIf loc c t f =>
         elift3 (EIf loc)
-               (ergo_map_expr ctx ctxt_new_variable_scope fn c)
-               (ergo_map_expr ctx ctxt_new_variable_scope fn t)
-               (ergo_map_expr ctx ctxt_new_variable_scope fn f)
+               (apply_map ctx c)
+               (apply_map ctx t)
+               (apply_map ctx f)
       | ELet loc n t v b =>
         elift2 (fun v' b' => (ELet loc) n t v' b')
-               (ergo_map_expr ctx ctxt_new_variable_scope fn v)
-               (ergo_map_expr (ctxt_new_variable_scope ctx n v) ctxt_new_variable_scope fn b)
+               (apply_map ctx v)
+               (apply_map (ctxt_new_variable_scope ctx n v) b)
       | EPrint loc v b =>
         elift2 (fun v' b' => (EPrint loc) v' b')
-               (ergo_map_expr ctx ctxt_new_variable_scope fn v)
-               (ergo_map_expr ctx ctxt_new_variable_scope fn b)
+               (apply_map ctx v)
+               (apply_map ctx b)
       | ERecord loc rs =>
         elift (ERecord loc)
               (fold_left
                  (fun ls nr =>
                     elift2 postpend ls
-                           (elift (fun x => (fst nr, x)) (ergo_map_expr ctx ctxt_new_variable_scope fn (snd nr))))
+                           (elift (fun x => (fst nr, x)) (apply_map ctx (snd nr))))
                  rs (esuccess nil nil))
       | ENew loc n rs =>
         elift (ENew loc n)
               (fold_left
                  (fun ls nr =>
                     elift2 postpend ls
-                           (elift (fun x => (fst nr, x)) (ergo_map_expr ctx ctxt_new_variable_scope fn (snd nr))))
+                           (elift (fun x => (fst nr, x)) (apply_map ctx (snd nr))))
                  rs (esuccess nil nil))
       | ECallFun loc fn' args =>
         elift (ECallFun loc fn')
               (fold_left (fun ls nv =>
-                            elift2 postpend ls (ergo_map_expr ctx ctxt_new_variable_scope fn nv))
+                            elift2 postpend ls (apply_map ctx nv))
                          args (esuccess nil nil))
       | ECallFunInGroup loc gn fn' args =>
         elift (ECallFunInGroup loc gn fn')
               (fold_left (fun ls nv =>
-                            elift2 postpend ls (ergo_map_expr ctx ctxt_new_variable_scope fn nv))
+                            elift2 postpend ls (apply_map ctx nv))
                          args (esuccess nil nil))
-      | EForeach loc rs whr fn' =>
-        elift3
-          (EForeach loc)
-          (fold_left
-             (fun ls nr =>
-                elift2 postpend ls
-                       (elift (fun x => (fst nr, x)) (ergo_map_expr ctx ctxt_new_variable_scope fn (snd nr))))
-             rs (esuccess nil nil))
-          (match whr with
-           | Some whr' => (elift Some) (ergo_map_expr ctx ctxt_new_variable_scope fn whr')
-           | None => esuccess None nil
-           end)
-          (ergo_map_expr ctx ctxt_new_variable_scope fn fn')
-
+      | EForeach loc rs whr r =>
+        let rs_ctx' :=
+            let proc_one ctx (nr:string * @ergo_expr A A' N) :=
+                let (n,e) := nr in
+                elift (fun x => (x,(ctxt_new_variable_scope ctx n e)))
+                      (elift (fun x => (n, x)) (apply_map ctx e))
+            in
+            elift_context_fold_left
+              proc_one rs ctx
+        in
+        eolift
+          (fun xy : list (string * @ergo_expr A A' N) * C =>
+             let (rs',ctx) := xy in
+             let whr' :=
+                 match whr with
+                 | Some whr' => (elift Some) (apply_map ctx whr')
+                 | None => esuccess None nil
+                 end
+             in
+             let r' := apply_map ctx r in
+             elift3 (EForeach loc) (esuccess rs' nil) whr' r')
+          rs_ctx'
       | EMatch loc expr pes def =>
         eolift
           (fun expr' =>
@@ -126,24 +135,26 @@ Section ErgoMap.
                                 match pe with
                                 | (CaseData _ _, e) =>
                                   elift (fun x => (fst pe, x))
-                                        (ergo_map_expr ctx ctxt_new_variable_scope fn e)
+                                        (apply_map ctx e)
                                 | (CaseWildcard _ _, e) =>
                                   elift (fun x => (fst pe, x))
-                                        (ergo_map_expr ctx ctxt_new_variable_scope fn e)
+                                        (apply_map ctx e)
                                 | (CaseLet _ name _, e) =>
                                   elift (fun x => (fst pe, x))
-                                        (ergo_map_expr (ctxt_new_variable_scope ctx name expr')
-                                                       ctxt_new_variable_scope fn e)
+                                        (ergo_map_expr
+                                           ctxt_new_variable_scope fn (ctxt_new_variable_scope ctx name expr')
+                                           e)
                                 | (CaseLetOption _ name _, e) =>
                                   elift (fun x => (fst pe, x))
-                                        (ergo_map_expr (ctxt_new_variable_scope ctx name expr')
-                                                       ctxt_new_variable_scope fn e)
+                                        (ergo_map_expr
+                                           ctxt_new_variable_scope fn (ctxt_new_variable_scope ctx name expr')
+                                           e)
                                 end
                                 prev)
                            (esuccess nil nil)
                            pes))
-               (ergo_map_expr ctx ctxt_new_variable_scope fn def))
-          (ergo_map_expr ctx ctxt_new_variable_scope fn expr)
+               (apply_map ctx def))
+          (apply_map ctx expr)
       end.
 
 End ErgoMap.
