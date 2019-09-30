9b5764d2faab3149bd1f514d600b4e1d985faa67
Jerome Simeon <jeromesimeon@me.com>
2019-09-30 10:25:32 -0400

    chore(dep) Upgrade to latest Q*cert, Concerto and Markdown Transform
    Signed-off-by: Jerome Simeon <jeromesimeon@me.com>

--- extraction/src/PrettyCommon.ml
+++ extraction/src/PrettyCommon.ml
@@ -478,7 +478,7 @@ let pretty_unary_op p sym callb ff u a =
   | OpOrderBy atts ->
       fprintf ff "@[<hv 0>%s%a(%a)@]" "sort" (pretty_squared_names sym) (List.map fst atts) (callb 0 sym) a
   | OpToString -> pretty_unary_exp sym callb "toString" ff a
-  | OpGenerateText -> pretty_unary_exp sym callb "generateText" ff a
+  | OpToText -> pretty_unary_exp sym callb "toText" ff a
   | OpLength -> pretty_unary_exp sym callb "stringLength" ff a
   | OpSubstring (n1,None) -> pretty_unary_exp sym callb ("substring["^(string_of_int n1)^"]") ff a
   | OpSubstring (n1,Some n2) -> pretty_unary_exp sym callb ("substring["^(string_of_int n1)^","^(string_of_int n2)^"]") ff a
