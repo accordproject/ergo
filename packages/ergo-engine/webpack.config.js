74f7f274c24bd98ef0867eb1981c250f7ee2fde6
Jerome Simeon <jeromesimeon@me.com>
2022-03-17 09:31:54 -0400

    fix(build): Update configuration for webpack 5
    Signed-off-by: Jerome Simeon <jeromesimeon@me.com>

--- packages/ergo-engine/webpack.config.js
+++ packages/ergo-engine/webpack.config.js
@@ -21,29 +21,29 @@ const packageJson = require('./package.json');
 module.exports = {
     entry: {
         client: [
-            './index.browser.js'
+            './index.dist.js'
         ]
     },
     output: {
         path: path.join(__dirname, 'umd'),
         filename: 'ergo-engine.js',
-        library: 'ergo-engine',
-        libraryTarget: 'umd',
-        umdNamedDefine: true
+        library: {
+            name: 'markdown-pdf',
+            type: 'umd',
+        },
+        umdNamedDefine: true,
     },
     plugins: [
-        new webpack.BannerPlugin(`Accord Project, Ergo v${packageJson.version} http://accordproject.org. Copyright 2018-2019, Clause Inc.
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.`),
+        new webpack.BannerPlugin(`Ergo v${packageJson.version}
+        Licensed under the Apache License, Version 2.0 (the "License");
+        you may not use this file except in compliance with the License.
+        You may obtain a copy of the License at
+        http://www.apache.org/licenses/LICENSE-2.0
+        Unless required by applicable law or agreed to in writing, software
+        distributed under the License is distributed on an "AS IS" BASIS,
+        WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+        See the License for the specific language governing permissions and
+        limitations under the License.`),
         new webpack.DefinePlugin({
             'process.env': {
                 'NODE_ENV': JSON.stringify('production')
@@ -55,12 +55,7 @@ limitations under the License.`),
             {
                 test: /\.js$/,
                 include: [path.join(__dirname, 'lib')],
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-env']
-                    }
-                }
+                use: ['babel-loader']
             },
             {
                 test: /\.ne$/,
@@ -68,9 +63,18 @@ limitations under the License.`),
             }
         ]
     },
-    node: {
-        fs: 'empty',
-        net: 'empty',
-        tls: 'empty'
+    resolve: {
+        fallback: {
+            'fs': false,
+            'tls': false,
+            'net': false,
+            'path': false,
+            'os': false,
+            'util': false,
+            'url': false,
+            'assert': require.resolve('assert/'),
+            'constants': require.resolve('constants-browserify'),
+            'stream': require.resolve('stream-browserify'),
+        }
     }
 };
